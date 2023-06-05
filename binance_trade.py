64038a64277138520228106b8aba270c48c42707
Andrey Elezov <113543774+ControlTheNarrative88@users.noreply.github.com>
2023-06-05 15:45:49 +0300

    update binance-trade

--- binance_trade.py
+++ binance_trade.py
@@ -2,20 +2,31 @@ from config import binance_api_key, binance_secret_key
 from binance import Client
 from openbb_terminal.sdk import openbb
 import pandas as pd
+import datetime
+
 
 client = Client(binance_api_key, binance_secret_key)
 
-class Coin:
-    def get_coin_data(self, limit=61):
-        # интервал обновления данных по цене можно менять, в данном случае раз в минуту
-        data = client.get_klines(symbol=self, interval="1m", limit=limit)
+def get_coin_data(self, limit=61):
 
-        df = pd.DataFrame()
+    data = client.get_klines(symbol=self, interval="1m", limit=limit)
 
-        # упаковываем в датафрейм для облегчения дальнейшей работы
-        for candle in range(limit):
-            date_time = datetime.datetime.fromtimestamp(data[candle][0] / 1e3)
-            close_price = float(data[candle][4])
-            new_df = pd.DataFrame({'Time': [date_time], f'{self}': [close_price]})
-            df = pd.concat([df, new_df], ignore_index=True)
-        return df
+    df = pd.DataFrame()
+
+    for candle in range(limit):
+        date_time = datetime.datetime.fromtimestamp(data[candle][0] / 1e3)
+        close_price = float(data[candle][4])
+        new_df = pd.DataFrame({'Time': [date_time], f'{self}': [close_price]})
+        df = pd.concat([df, new_df], ignore_index=True)
+    return df
+    
+def make_trade(ticker, buy_value,side, type="MARKET"):
+
+    buy_value = buy_value
+    df = get_coin_data(ticker)
+    quantity = round(buy_value / df.iloc[-1][ticker])
+    trade_status = client.create_order(symbol=ticker, side=side, type=type, quantity= quantity)
+    return trade_status
+
+
+make_trade("LUNAUSDT", 10, "SELL")
